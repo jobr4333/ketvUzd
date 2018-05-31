0e3a0a4d5bb3c279e3a42dd9a060a8c0063e0bd1
Jonas Br <jonas.bruzga@mif.stud.vu.lt>
2018-05-31 11:44:24 +0300

    pataisytas vektorius

--- Project1/Vector.cpp
+++ Project1/Vector.cpp
@@ -1,40 +1,37 @@
 #pragma once
 
 #include "Vector.h"; //supeeeeer
-#include "Header.h"; //shitty
 #include <iostream>
 #include <chrono>
 #include <vector>//basic
+#include <math.h>
 
 int main() {
-	int i, x = 100000;
+	int i;
+	unsigned long long int x = 10000;
 	vector<int> v1;
 	std::vector<int> v2;
-	Vector<int> v3;
 
-	std::cout << x << std::endl;
+	
 
-	auto start = std::chrono::high_resolution_clock::now(); //laiko matavimo start
-	for (i = 0; i < x; ++i)
-		v1.push_back(i);
-	auto finish = std::chrono::high_resolution_clock::now(); //laiko matavimo start
-	std::chrono::duration<double> elapsed = finish - start;
-	std::cout << "turbo vector finish: " << elapsed.count() << std::endl;
+	for (size_t y = 1; y < 5; y++) {
+		std::cout << "Number of elements (pushbacks): " << x << std::endl << std::endl;
+		auto start = std::chrono::high_resolution_clock::now(); //laiko matavimo start
+		for (i = 0; i < x; ++i)
+			v1.push_back(i);
+		auto finish = std::chrono::high_resolution_clock::now(); //laiko matavimo start
+		std::chrono::duration<double> elapsed = finish - start;
+		std::cout << "turbo vector finish: " << elapsed.count() << std::endl;
 
+		start = std::chrono::high_resolution_clock::now(); //laiko matavimo start
+		for (i = 0; i < x; ++i)
+			v2.push_back(i);
+		finish = std::chrono::high_resolution_clock::now(); //laiko matavimo start
+		elapsed = finish - start;
 
-	start = std::chrono::high_resolution_clock::now(); //laiko matavimo start
-	for (i = 0; i < x; ++i)
-		v2.push_back(i);
-	finish = std::chrono::high_resolution_clock::now(); //laiko matavimo start
-	 elapsed = finish - start;
-	std::cout << "std vector finish: " << elapsed.count() << std::endl;
-
-	start = std::chrono::high_resolution_clock::now(); //laiko matavimo start
-	for (i = 0; i < x; ++i)
-		v3.push_back(i);
-	finish = std::chrono::high_resolution_clock::now(); //laiko matavimo start
-	elapsed = finish - start;
-	std::cout << "forum solution vector finish: " << elapsed.count() << std::endl;
-	std::cin >> x;
+		std::cout << "std vector finish:   " << elapsed.count() << std::endl << std::endl;
+		x = x * 10;
+		
+	}
 	return 0;
 }
